7118e8f7850cbef41528f6d37024f8890de5b2d9
Axl Crow <aj.ayte@uniandes.edu.co>
2023-05-14 12:02:37 -0500

    fix: correct parameter to check in the bulk create

--- measurements/measurements/views.py
+++ measurements/measurements/views.py
@@ -50,7 +50,7 @@ def MeasurementsCreate(request):
         data_json = json.loads(data)
         measurement_list = []
         for measurement in data_json:
-                    if check_variable(data_json) and check_place(data_json):
+                    if check_variable(measurement) and check_place(measurement):
                         db_measurement = Measurement()
                         db_measurement.variable = measurement['variable']
                         db_measurement.value = measurement['value']
